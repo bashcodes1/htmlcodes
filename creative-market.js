349ba8808b56142fced01edcaa0303c66ee6ee5d
bashcodes1 <118808901+bashcodes1@users.noreply.github.com>
2022-12-25 13:49:40 +0100

    keybloom project

--- creative-market.js
+++ creative-market.js
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
+
 const bar = document .getElementById('bar');
 const nav = document .getElementById('navbar'); 
-
+const close = document.getElementById('close');
 if (bar) {
   bar.addEventListener('click',() =>{
     nav.classList.add('active');
   })
   
-=======
-const bar = document .getElementById('bar');
-const nav = document .getElementById('navbar'); 
 
-if (bar) {
-  bar.addEventListener('click',() =>{
-    nav.classList.add('active');
+}
+
+if(close){
+  close.addEventListener('click',() =>{
+    nav.classList.remove('active');
   })
-  
->>>>>>> 4bab7ac1607ed69f274d2c330d1c90a28d47bf44
 }
